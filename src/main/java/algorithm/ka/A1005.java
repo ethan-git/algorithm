b2b16c9a0c06d3e74e49a7785aa0be04a97642bf
ethan <eungjun01@naver.com>
2018-03-23 22:17:19 +0900

    add case

--- src/main/java/algorithm/ka/A1005.java
+++ src/main/java/algorithm/ka/A1005.java
@@ -12,7 +12,6 @@
 package algorithm.ka;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author ejpark
@@ -21,31 +20,49 @@ import java.util.List;
 public class A1005 {
 
 	public static void main(String[] args) {
-		int[] heights = {4, 6};
-		int[] widths = {5, 6};
-		String[][] boards = {{"CCBDE", "AAADE", "AAABF", "CCBBF"},{"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"}};
-
-		for(int i=0; i<heights.length; i++) {
+		int[] heights = { 4, 6 };
+		int[] widths = { 5, 6 };
+		String[][] boards = { { "CCBDE", "AAADE", "AAABF", "CCBBF" },
+				{ "TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ" } };
+		for (int i = 0; i < heights.length; i++) {
 			int height = heights[i];
 			int width = widths[i];
 			String[] boardArr = boards[i];
 			char[][] board = new char[height][width];
-			for(int j=0; j<height; j++) {
-				String line = boardArr[j];
-				for(int k=0; k<width; k++) {
-					board[j][k] = line.charAt(k);
+			for (int j = 0; j < height; j++) {
+				char[] lines = boardArr[j].toCharArray();
+				for (int k = 0; k < width; k++) {
+					board[j][k] = lines[k];
 				}
 			}
-			while(true) {
-				List<Integer[]> matchedPoint = getMatchedPoint(board, height, width);
-				if(matchedPoint.size()==0) {
+			Object[] listBoard = new Object[width];
+			for (int k = 0; k < width; k++) {
+				listBoard[k] = new ArrayList<Object>();
+				for (int j = 0; j < height; j++) {
+
+				}
+			}
+			while (true) {
+				char[][] remove = scan(board, height, width);
+				if (remove == null) {
 					break;
 				}
-				removePoint(matchedPoint, board);
+				for (int j = 0; j < height; j++) {
+					for (int k = 0; k < width; k++) {
+						if (remove[j][k] == '*') {
+							if (j == 0) {
+								board[j][k] = '*';
+							} else {
+								board[j][k] = board[j - 1][k];
+								board[j - 1][k] = '*';
+							}
+						}
+					}
+				}
 			}
 			int cnt = 0;
-			for(int j=0; j<height; j++) {
-				for(int k=0; k<width; k++) {
+			for (int j = 0; j < height; j++) {
+				for (int k = 0; k < width; k++) {
 					if(board[j][k] == '*') {
 						cnt++;
 					}
@@ -55,38 +72,28 @@ public class A1005 {
 		}
 	}
 
-	private static void removePoint(List<Integer[]> matchedPoint, char[][] board) {
-		for(Integer[] points : matchedPoint) {
-			int h = points[0];
-			int w = points[1];
-			board[h][w] = '*';
-			board[h][w+1] = '*';
-			board[h+1][w] = '*';
-			board[h+1][w+1] = '*';
-			if(h>0) {
-				board[h+1][w] = board[h-1][w];
-				board[h+1][w+1] = board[h-1][w+1];
-				board[h-1][w] = '*';
-				board[h-1][w+1] = '*';
-			}
-		}
-	}
-
-	private static List<Integer[]> getMatchedPoint(char[][] board, int height, int width) {
-		List<Integer[]> matchedPoint = new ArrayList<Integer[]>();
-		for(int j=0; j<height-1; j++) {
-			for(int k=0; k<width-1; k++) {
+	private static char[][] scan(char[][] board, int height, int width) {
+		char[][] remove = new char[height][width];
+		boolean isRemoved = false;
+		for (int j = 0; j < height-1; j++) {
+			for (int k = 0; k < width-1; k++) {
 				char first = board[j][k];
-				char second = board[j][k+1];
-				char third = board[j+1][k];
-				char fourth = board[j+1][k+1];
-				if(first!='*' && first==second && first==second && first==third && first==fourth) {
-					Integer[] point = {j,k};
-					matchedPoint.add(point);
+				char second = board[j][k + 1];
+				char third = board[j + 1][k];
+				char fourth = board[j + 1][k + 1];
+				if (first != '*' && first == second && first == second && first == third && first == fourth) {
+					remove[j][k] = '*';
+					remove[j][k + 1] = '*';
+					remove[j + 1][k] = '*';
+					remove[j + 1][k + 1] = '*';
+					isRemoved = true;
 				}
 			}
 		}
-		return matchedPoint;
+		if (!isRemoved) {
+			return null;
+		}
+		return remove;
 	}
 }
 /*
